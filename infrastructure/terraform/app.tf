4af27ea0d180486eef3ecc143e487fc354e68962
Brian Denicola <bjd145@yahoo.com>
2021-01-15 15:16:34 -0600

    Cognitive Services

--- infrastructure/terraform/app.tf
+++ infrastructure/terraform/app.tf
@@ -288,13 +288,9 @@ resource "azurerm_private_endpoint" "acr_account" {
 
 resource "azurerm_cognitive_account" "traduire_app" {
   name                = "${var.application_name}-cogs01"
-  location            = azurerm_resource_group.traduire_app.location
   resource_group_name = azurerm_resource_group.traduire_app.name
-  kind                = "Speech"
+  location            = azurerm_resource_group.traduire_app.location
+  kind                = "SpeechServices"
 
-  sku_name = "S0"
-
-  tags = {
-    Acceptance = "Test"
-  }
+  sku_name            = "S0"
 }
