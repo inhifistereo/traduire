4ae0eba84b7a8565342c3a47b565201a34b1cbd4
Brian Denicola <bjd145@yahoo.com>
2021-01-20 10:33:30 -0600

    Helm chart for Dapr testing

--- infrastructure/deploy_application.ps1
+++ infrastructure/deploy_application.ps1
@@ -31,13 +31,18 @@ function Connect-Azure
     az account set -s $SubscriptionName -o none
 }
 
-Set-Variable -Name DAPR_VERSION -Value "1.0.0-rc.2" -Option Constant
-Set-Variable -Name KEDA_VERSION -Value "1.5.0" -Option Constant
-Set-Variable -Name APP_RG_NAME  -Value ("{0}_app_rg" -f $AppName)   -Option Constant
-Set-Variable -Name APP_K8S_NAME -Value ("{0}-aks01" -f $AppName)    -Option Constant
-Set-Variable -Name APP_ACR_NAME -Value ("{0}acr01" -f $AppName)     -Option Constant
+Set-Variable -Name DAPR_VERSION -Value "1.0.0-rc.2"                     -Option Constant
+Set-Variable -Name KEDA_VERSION -Value "1.5.0"                          -Option Constant
+Set-Variable -Name APP_RG_NAME  -Value ("{0}_app_rg" -f $AppName)       -Option Constant
+Set-Variable -Name APP_K8S_NAME -Value ("{0}-aks01" -f $AppName)        -Option Constant
+Set-Variable -Name APP_ACR_NAME -Value ("{0}acr01" -f $AppName)         -Option Constant
+Set-Variable -Name APP_KV_NAME  -Value ("{0}-kv01" -f $AppName)         -Option Constant
+Set-Variable -Name APP_MSI_NAME -Value ("{0}-dapr-reader" -f $AppName)  -Option Constant
 
-Start-Docker
+$cwd = $PWD.Path
+Set-location -Path ..\deploy
+
+#Start-Docker
 Connect-Azure
 
 #Set Subscription and login into ACR
@@ -45,6 +50,16 @@ az acr login -n $APP_ACR_NAME
 
 #Get AKS Credential file
 az aks get-credentials -n $APP_K8S_NAME -g $APP_RG_NAME
+
+#Get MSI Account
+$ms_resource_id = az identity show -n $APP_MSI_NAME -g $APP_RG_NAME --query id -o tsv
+$ms_client_id = az identity show -n $APP_MSI_NAME -g $APP_RG_NAME --query clientId -o tsv
+
+#Get Service Bus Connection String
+#TBC
+
+#Set Key Vault Secret for Connection Strings
+#TBC
 
 # Install Traefik Ingress 
 helm repo add traefik https://helm.traefik.io/traefik    
@@ -60,7 +75,10 @@ helm upgrade -i keda kedacore/keda --namespace keda --version $KEDA_VERSION
 helm repo add dapr https://dapr.github.io/helm-charts
 helm repo update
 kubectl create namespace dapr-system
-helm upgrade -i dapr dapr/dapr --namespace dapr-system --version $DAPR_VERSION
+helm upgrade -i dapr dapr/dapr --namespace dapr-system --version $DAPR_VERSION --wait
+
+#Due to https://github.com/dapr/dapr/issues/1621#
+#kubectl -n dapr-system rollout restart deployment dapr-sidecar-injector
 
 # Install Pod Identity 
 helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
@@ -68,4 +86,15 @@ helm repo update
 helm upgrade -i aad-pod-identity aad-pod-identity/aad-pod-identity
 
 # Install App
-# helm upgrade --install --set key=value traduire .
+helm upgrade -i `
+   --set app_name=$AppName `
+   --set msi_client_id=$ms_client_id `
+   --set msi_resource_id=$ms_resource_id `
+   --set keyvault_name=$APP_KV_NAME `
+   traduire . 
+
+#Testing from within utils containers
+#Invoke-RestMethod -Method Post -Uri "http://localhost:3500/v1.0/bindings/vxzjl-servicebus" -Body '{ "data": { "message": "bye" }, "operation": "message" }'
+#Invoke-RestMethod -Uri "http://localhost:3500/v1.0/secrets/vxzjl-vault"
+
+Set-location -Path $cwd
