4d4aa85acc1339ef5ff0f1d6bb19cf31e8bacc90
Brian Denicola <bjd145@yahoo.com>
2021-01-21 10:39:39 -0600

    Fixed posgresql userid in secret

--- infrastructure/terraform/app.tf
+++ infrastructure/terraform/app.tf
@@ -332,6 +332,6 @@ resource "azurerm_key_vault_secret" "storage_secret_name" {
 
 resource "azurerm_key_vault_secret" "postgresql_connection_string" {
   name         = var.postgresql_secret_name
-  value        = "postgres://${azurerm_postgresql_server.traduire_app.administrator_login}:${azurerm_postgresql_server.traduire_app.administrator_login_password}@${azurerm_postgresql_server.traduire_app.name}.postgres.database.azure.com:5432/${var.postgresql_database_name}?sslmode=verify-ca"
+  value        = "postgres://${azurerm_postgresql_server.traduire_app.administrator_login}@${azurerm_postgresql_server.traduire_app.name}:${azurerm_postgresql_server.traduire_app.administrator_login_password}@${azurerm_postgresql_server.traduire_app.name}.postgres.database.azure.com:5432/${var.postgresql_database_name}?sslmode=verify-ca"
   key_vault_id = azurerm_key_vault.traduire_app.id
 }
