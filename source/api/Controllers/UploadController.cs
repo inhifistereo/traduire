7b7f7eac1b391df86d52529b9a8f74b8e295594f
Brian Denicola <bjd145@yahoo.com>
2021-02-09 08:53:48 -0600

    Publish to Service Bus/PubSub

--- source/api/Controllers/UploadController.cs
+++ source/api/Controllers/UploadController.cs
@@ -22,6 +22,7 @@ namespace TraduireAPi.Controllers
     public class UploadController : ControllerBase
     {
         private readonly string BlobStoreName = Environment.GetEnvironmentVariable("DAPR_BLOB_STORE_NAME", EnvironmentVariableTarget.Process);
+        private readonly string PubSubName = Environment.GetEnvironmentVariable("DAPR_PUBSUB_NAME", EnvironmentVariableTarget.Process);
 
         private readonly ILogger _logger;
 
@@ -31,25 +32,37 @@ namespace TraduireAPi.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Guid>> Post(IFormFile file, CancellationToken cancellationToken, [FromServices] DaprClient daprClient)
+        public async Task<ActionResult> Post(IFormFile file, CancellationToken cancellationToken, [FromServices] DaprClient daprClient)
         {
             try{
                 var id = Guid.NewGuid().ToString();
-                
-                var metadata = new Dictionary<string, string>();
-                metadata.Add("blobName", WebUtility.HtmlEncode(file.FileName));
-                metadata.Add("Custom", id);
+                var safeFileName = WebUtility.HtmlEncode(file.FileName); 
 
-                await daprClient.InvokeBindingAsync(BlobStoreName, "create",  await ConvertFileToBase64Encoding(file), metadata, cancellationToken);                      
-                
-                _logger.LogInformation($"{id} was successfullly as {file.FileName}");
+                var metadata = new Dictionary<string, string>();
+                metadata.Add("blobName", safeFileName);
+                metadata.Add("transactionId", id);
+
+                var response = await daprClient.InvokeBindingAsync<string,BlobBindingResponse>(BlobStoreName, "create",  await ConvertFileToBase64Encoding(file), metadata, cancellationToken);                      
+                _logger.LogInformation($"{id} was successfullly saved as {safeFileName}");
+
+                var eventdata = new {
+                    timeStamp = DateTime.UtcNow.ToString("s"),
+                    transactionId = id,
+                    fileName = safeFileName,
+                    fileUri = response.blobURL
+                };
+
+                await daprClient.PublishEventAsync(PubSubName, "registertanscription", eventdata, cancellationToken );
+                _logger.LogInformation($"{id} was was published to {PubSubName} pub sub store");
                 return Ok(id); 
             }
             catch( Exception ex ) {
-                _logger.LogWarning($"Failed to create {file.FileName} - {ex.Message}");
+                //Add Compensating tranasaction to undo error
+
+                _logger.LogWarning($"Failed to create {file.FileName} - {ex.Message}");    
             }
 
-            return Ok(null);
+            return BadRequest();
         }
 
         private async Task<string> ConvertFileToBase64Encoding( IFormFile f )
@@ -66,5 +79,9 @@ namespace TraduireAPi.Controllers
 
             return String.Empty;
         }
+    }
+
+    public class BlobBindingResponse {
+        public string blobURL { get; set; }
     }
 }
