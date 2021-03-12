b365059c65a013447de766969ce782f4021f9c2c
Brian Denicola <bjd145@yahoo.com>
2021-03-12 13:31:53 -0600

    Toast dialog box for upload

--- source/ui/src/Components/Uploader/Uploader.tsx
+++ source/ui/src/Components/Uploader/Uploader.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './Uploader.css';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+import Toast from 'react-bootstrap/Toast';
 
 type Props = {
 	selectedFile: File,
@@ -10,33 +12,42 @@ type Props = {
 
 type State = {
 	isLoading: boolean,
+	showToast: boolean,
+	toastBody: string
 }
 
 class Uploader extends Component<Props,State> {
 	constructor(props:any) {
 		super(props);
 		this.state = {
-			isLoading: false
+			isLoading: false,
+			showToast: false,
+			toastBody: ""
 		}
 	}
 
+	private setToastState(isShowing:boolean, msg: string = ""){
+		this.setState({
+			showToast: isShowing,
+			toastBody: msg
+		})
+	}
+
 	private uploadFileRequest = async () => {
-		/*const response = await fetch(this.props.uploadFileUri, {
+		const response = await fetch(this.props.uploadFileUri, {
 			method: 'POST',
 			body: this.props.selectedFile
-		});*/
-
-		console.log(this.props.uploadFileUri);
-		console.log(this.props.selectedFile);
-
-		//return response;
-		return new Promise((resolve) => setTimeout(resolve, 5000));
+		});
+		
+		return response;
 	}
 
   	private uploadFile = async () => {
 	  this.setState({ isLoading: true })
 	  try{
-		await this.uploadFileRequest();
+		var response = await this.uploadFileRequest();
+		var msg = this.props.selectedFile.name + " upload status - " + response.status + ": " + response.statusText;
+		this.setToastState(true, msg);
 	  }
 	  finally {
 		this.setState({ isLoading: false })
@@ -45,11 +56,22 @@ class Uploader extends Component<Props,State> {
 
   	render() {
 		const selectedFile = this.props.selectedFile;
+		const showToast = this.state.showToast;
+		const toastBody = this.state.toastBody;
+
 	  	return ( 
 			<div>
 				<Button variant="primary" disabled={this.state.isLoading} size="lg" block onClick={this.uploadFile} >
 					{this.state.isLoading ? 'Uploading ' : 'Upload ' + selectedFile.name }
 				</Button>
+				<div style={{position: 'absolute', bottom: 0, right: 0}} >
+					<Toast onClose={() => this.setToastState(false)} show={showToast} delay={3000} autohide >
+						<Toast.Header>
+							<strong className="mr-auto">Upload Status</strong>
+						</Toast.Header>
+						<Toast.Body>{toastBody}</Toast.Body>
+					</Toast>
+				</div>
 			</div>
   		);
   	}
