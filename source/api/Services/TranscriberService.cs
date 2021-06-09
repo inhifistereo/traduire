191c52537561ca2be39ae566c72b554547a6f288
Brian Denicola <brian@briandenicola.com>
2021-06-09 20:27:18 +0000

    Added sample reply message

--- source/api/Services/TranscriberService.cs
+++ source/api/Services/TranscriberService.cs
@@ -17,7 +17,18 @@ namespace traduire.webapi
 
         public override Task<TranscriptionReply> TranscribeAudio(TranscriptionRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new TranscriptionReply());
+            return Task.FromResult(new TranscriptionReply {
+                TranscriptionId = Guid.NewGuid().ToString(),
+                CreateTime = DateTime.UtcNow.ToString(),
+                LastUpdateTime = DateTime.UtcNow.ToString(),
+                Status = "Running",
+                FileName = "TBD",
+                BlobUri = request.BlobUri,
+                TranscriptionStatusUri = string.Empty,
+                TranscriptionTextUri = string.Empty,
+                TranscriptionText = string.Empty,
+                StatusDetails = string.Empty
+            });
         }
     }
 }
