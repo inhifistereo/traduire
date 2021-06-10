ef40e1ccf64479f4670fd918bff4a1dfde5b7322
Brian Denicola <brian@briandenicola.com>
2021-06-10 02:44:17 +0000

    Working example

--- source/api/Services/TranscriberService.cs
+++ source/api/Services/TranscriberService.cs
@@ -4,91 +4,71 @@ using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using Dapr.Client;
+
+using transcription.models;
+using transcription.api.dapr;
 
 namespace traduire.webapi
 {
     public class TranscriberService : Transcriber.TranscriberBase
     {
         private readonly ILogger<TranscriberService> _logger;
+        private readonly DaprHelper _daprClient;
         public TranscriberService(ILogger<TranscriberService> logger)
         {
+            _daprClient = new DaprHelper( new DaprClientBuilder().Build() );
             _logger = logger;
         }
-        public override Task<TranscriptionReply> TranscribeAudio(TranscriptionRequest request, ServerCallContext context)
+
+        private TranscriptionReply TranscriptionReplyFactory(string id, string status, string uri, string createdTime, string transcription)
         {
-            return Task.FromResult(new TranscriptionReply {
-                TranscriptionId = Guid.NewGuid().ToString(),
-                CreateTime = DateTime.UtcNow.ToString(),
+            return new TranscriptionReply {
+                TranscriptionId = id,
+                CreateTime = createdTime,
                 LastUpdateTime = DateTime.UtcNow.ToString(),
-                Status = "TBD",
-                FileName = "TBD",
-                BlobUri = request.BlobUri,
-                TranscriptionStatusUri = string.Empty,
-                TranscriptionTextUri = string.Empty,
-                TranscriptionText = string.Empty,
-                StatusDetails = string.Empty
-            });
+                Status = status,
+                BlobUri = uri,
+                TranscriptionText = transcription
+            };
         }
 
         public override async Task TranscribeAudioStream(TranscriptionRequest request, IServerStreamWriter<TranscriptionReply> responseStream, ServerCallContext context)
         {
-            await responseStream.WriteAsync(new TranscriptionReply {
-                TranscriptionId = Guid.NewGuid().ToString(),
-                CreateTime = DateTime.UtcNow.ToString(),
-                LastUpdateTime = DateTime.UtcNow.ToString(),
-                Status = "Started",
-                FileName = "TBD",
-                BlobUri = request.BlobUri,
-                TranscriptionStatusUri = string.Empty,
-                TranscriptionTextUri = string.Empty,
-                TranscriptionText = string.Empty,
-                StatusDetails = string.Empty
-            });
+            var TranscriptionId = Guid.NewGuid(); 
+            var createdTime = DateTime.UtcNow.ToString();
 
-            await Task.Delay(500);
+            _logger.LogInformation($"Transcription request was received.");
+            try{
 
-            await responseStream.WriteAsync(new TranscriptionReply {
-                TranscriptionId = Guid.NewGuid().ToString(),
-                CreateTime = DateTime.UtcNow.ToString(),
-                LastUpdateTime = DateTime.UtcNow.ToString(),
-                Status = "SentToCognitiveServices",
-                FileName = string.Empty,
-                BlobUri = request.BlobUri,
-                TranscriptionStatusUri = string.Empty,
-                TranscriptionTextUri = string.Empty,
-                TranscriptionText = string.Empty,
-                StatusDetails = string.Empty
-            });
+                var state = await _daprClient.UpdateState(TranscriptionId, request.BlobUri);
+                _logger.LogInformation($"{TranscriptionId}. Record was successfullly saved as to {Components.StateStoreName} State Store");
+                await responseStream.WriteAsync( TranscriptionReplyFactory(TranscriptionId.ToString(), TraduireTranscriptionStatus.Pending.ToString(), request.BlobUri, createdTime, string.Empty ));
 
-            await Task.Delay(500);
+                await _daprClient.PublishEvent( TranscriptionId, request.BlobUri, context.CancellationToken );
+                _logger.LogInformation($"{TranscriptionId}. {request.BlobUri} was successfullly published to {Components.PubSubName} pubsub store");
+                await responseStream.WriteAsync( TranscriptionReplyFactory(TranscriptionId.ToString(), TraduireTranscriptionStatus.SentToCognitiveServices.ToString(), request.BlobUri, createdTime, string.Empty ));
 
-            await responseStream.WriteAsync(new TranscriptionReply {
-                TranscriptionId = Guid.NewGuid().ToString(),
-                CreateTime = DateTime.UtcNow.ToString(),
-                LastUpdateTime = DateTime.UtcNow.ToString(),
-                Status = "Pending",
-                FileName = string.Empty,
-                BlobUri = request.BlobUri,
-                TranscriptionStatusUri = string.Empty,
-                TranscriptionTextUri = string.Empty,
-                TranscriptionText = string.Empty,
-                StatusDetails = string.Empty
-            });
+                TraduireTranscription currentState;
+                do {
+                    await Task.Delay(TimeSpan.FromSeconds(30));
 
-            await Task.Delay(500);
+                    currentState = await _daprClient.GetState(TranscriptionId);
 
-            await responseStream.WriteAsync(new TranscriptionReply {
-                TranscriptionId = Guid.NewGuid().ToString(),
-                CreateTime = DateTime.UtcNow.ToString(),
-                LastUpdateTime = DateTime.UtcNow.ToString(),
-                Status = "Completed",
-                FileName = string.Empty,
-                BlobUri = request.BlobUri,
-                TranscriptionStatusUri = string.Empty,
-                TranscriptionTextUri = string.Empty,
-                TranscriptionText = string.Empty,
-                StatusDetails = string.Empty
-            });
+                    _logger.LogInformation($"{TranscriptionId}. Transcription status is {currentState.Status}");
+                    await responseStream.WriteAsync( TranscriptionReplyFactory(TranscriptionId.ToString(), currentState.Status.ToString(), request.BlobUri, createdTime, string.Empty ));
+
+                } while( currentState.Status != TraduireTranscriptionStatus.Completed );
+
+                _logger.LogInformation($"{TranscriptionId}. Attempting to download completed transcription"); 
+                await responseStream.WriteAsync( TranscriptionReplyFactory(TranscriptionId.ToString(), TraduireTranscriptionStatus.Completed.ToString(), request.BlobUri, createdTime, currentState.TranscriptionText ));
+            }
+            catch( Exception ex ) 
+            {
+                _logger.LogWarning($"Failed to transcribe {request.BlobUri} - {ex.Message}");        
+                await responseStream.WriteAsync( TranscriptionReplyFactory(TranscriptionId.ToString(), TraduireTranscriptionStatus.Failed.ToString(), request.BlobUri, createdTime, string.Empty ));            
+            }
+            
         }
     }
 }
