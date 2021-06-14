f9609ba540bf12487bd76a0b33224ab037fbf6ac
Brian Denicola <brian@briandenicola.com>
2021-06-13 20:37:07 -0500

    Restoring UI storage account

--- infrastructure/terraform/ui.tf
+++ infrastructure/terraform/ui.tf
@@ -16,3 +16,16 @@ resource "azurerm_static_site" "traduire_ui" {
   resource_group_name   = azurerm_resource_group.traduire_ui.name
   location              = "centralus"
 }
+
+resource "azurerm_storage_account" "traduire_storage_ui" {
+  name                     = var.ui_storage_name
+  resource_group_name      = azurerm_resource_group.traduire_ui.name
+  location                 = azurerm_resource_group.traduire_ui.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  static_website {
+    error_404_document    = "404.html"
+    index_document        = "index.html"
+  }
+} 
