bd82188d7d16d865bf89799b1f0341eb17d16b44
Brian Denicola <brian@briandenicola.com>
2021-06-15 13:11:27 -0500

    Bug fixes on WebSocket support

--- source/ui/src/Components/Transcription/Transcription.tsx
+++ source/ui/src/Components/Transcription/Transcription.tsx
@@ -41,11 +41,13 @@ type TranscriptionMessage = {
 	lastUpdated: string
 }
 
-const hubName = "transcription"
 var ws:WebSocket;
+var hubName:string = "transcription";
 
-class Transcription extends Component<Props,State> {
-	constructor(props:any) {
+class Transcription extends Component<Props,State> 
+{
+	constructor(props:any) 
+	{
 		super(props);
 		this.state = {
 			isLoading: false,
@@ -56,54 +58,40 @@ class Transcription extends Component<Props,State> {
 			serviceClient: new WebPubSubServiceClient(this.props.webpubSubUri, new AzureKeyCredential(this.props.webpubSubKey), hubName)
 		}
 
-		this.state.serviceClient.getAuthenticationToken().then( token => {
-			ws = new WebSocket(token.url);
-			ws.onmessage = (event:MessageEvent) => {
-				let msg = event.data as TranscriptionMessage
-				this.updateState(msg);
-			};
-		});	
-
+		this.configWSConnection = this.configWSConnection.bind(this);
 	}
 
-	//Hack. Please refact. 
-	private getStatusMessage = (status: number) => {
-		if( status === 0 ) {
-			return "Waiting to be picked up";
-		}
-		else if ( status === 1 ) {
-			return "Sent to CognitiveServices";
-		}
-		else if ( status === 2 ) {
-			return "Working on Transcription";
-		}
-		else if ( status === 3 ) {
-			return "Completed";
-		}
-		else {
-			return "Failed";
-		}		
+	private configWSConnection = async () => 
+	{
+		var token = await this.state.serviceClient.getAuthenticationToken();
+		ws = new WebSocket(token.url);
+		ws.onmessage = (event:MessageEvent) => {
+			let msg = JSON.parse(event.data);
+			this.setState({
+				transcriptionStatus: `${msg.StatusMessage} [${new Date(msg.LastUpdated)}]`
+			})
+		};
 	}
 
-	private replaceUri = (uri: string, id: string) => {
+	private replaceUri = (uri: string, id: string) => 
+	{
 		return uri.replace("{0}", id);
 	}
 
-	private updateState = (msg: TranscriptionMessage) => {
+	private updateState = (msg: TranscriptionMessage) => 
+	{
 		this.setState({
 			transcriptionId: msg.transcriptionId,
-			transcriptionStatus: `${this.getStatusMessage(msg.statusMessage)} [${msg.lastUpdated}]`
 		})
 	}
 
-	private checkStatus = async () =>
+	private checkStatus = async () => 
 	{
 		var uri = this.replaceUri(this.props.statusUri, this.state.transcriptionId);
 		const response = await fetch(uri);
 		var body = await response.json().then(data => data as TranscriptionMessage);
-		this.updateState(body);
 
-		if(body.statusMessage === 3 ) {
+		if(body.statusMessage === 3) {
 			await this.getTranscription();
 		}
 	}
@@ -126,6 +114,8 @@ class Transcription extends Component<Props,State> {
 			method: 'POST',
 			body:  formData
 		});
+
+		await this.configWSConnection();
 
 		return response;
 	}
@@ -166,7 +156,7 @@ class Transcription extends Component<Props,State> {
 				<hr/>
 				<Container>
 					<Row>
-						<Col><Button variant="secondary" size="lg" block onClick={this.checkStatus}>Check Status</Button></Col>
+						<Col><Button variant="secondary" size="lg" block onClick={this.checkStatus}>Get Transcription</Button></Col>
 						<Col><Alert variant="info">{transcriptionStatus}</Alert></Col>
 					</Row>
 					<Row>
