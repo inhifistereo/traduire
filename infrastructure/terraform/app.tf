0573f3981867594a89cb3070e5691d7637b6ab36
Brian Denicola <brian@briandenicola.com>
2021-09-10 19:17:09 +0000

    Moved to private storage container

--- infrastructure/terraform/app.tf
+++ infrastructure/terraform/app.tf
@@ -105,7 +105,7 @@ resource "azurerm_storage_account" "traduire_app" {
 resource "azurerm_storage_container" "mp3" {
   name                  = "mp3files"
   storage_account_name  = azurerm_storage_account.traduire_app.name
-  container_access_type = "blob"
+  container_access_type = "private"
 }
 
 resource "azurerm_private_endpoint" "storage_account" {
