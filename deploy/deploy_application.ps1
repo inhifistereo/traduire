c45094759615c6d031ffcdb3bc5c53bec993bac6
Brian Denicola <brian@briandenicola.com>
2021-12-08 04:35:01 +0000

    Final upgrade to 1.5.1

--- deploy/deploy_application.ps1
+++ deploy/deploy_application.ps1
@@ -15,7 +15,7 @@ param(
 
 . .\modules\traduire_functions.ps1
 
-Set-Variable -Name DAPR_VERSION         -Value "1.4.4"                           -Option Constant
+Set-Variable -Name DAPR_VERSION         -Value "1.5.1"                           -Option Constant
 Set-Variable -Name KEDA_VERSION         -Value "2.3.0"                           -Option Constant
 Set-Variable -Name CERT_MGR_VERSION     -Value "v1.6.1"                          -Option Constant
 Set-Variable -Name APP_RG_NAME          -Value ("{0}_app_rg" -f $AppName)        -Option Constant
