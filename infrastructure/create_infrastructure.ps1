d6347a1949c3463a5246b5de1d24c78deaa65cd6
briandenicola <brian@briandenicola.com>
2022-10-20 13:26:39 -0500

    Bug fix on DAPR Identity configuration values

--- infrastructure/create_infrastructure.ps1
+++ infrastructure/create_infrastructure.ps1
@@ -23,8 +23,8 @@ $KEDA_RESOURCEID=$(terraform -chdir=terraform output -raw KEDA_RESOURCE_ID)
 az aks pod-identity add --resource-group $AKS_RESOURCE_GROUP --cluster-name $CLUSTER_NAME --namespace $KEDA_NAMESPACE --name $KEDA_IDENTITY_NAME --identity-resource-id $KEDA_RESOURCEID
 
 $DEFAULT_NAMESPACE="default"
-$KEDA_IDENTITY_NAME=$(terraform -chdir=terraform output -raw DAPR_MI_NAME)
-$KEDA_RESOURCEID=$(terraform -chdir=terraform output -raw DAPR_RESOURCE_ID)
+$DAPR_IDENTITY_NAME=$(terraform -chdir=terraform output -raw DAPR_MI_NAME)
+$DAPR_RESOURCEID=$(terraform -chdir=terraform output -raw DAPR_RESOURCE_ID)
 az aks pod-identity add --resource-group $AKS_RESOURCE_GROUP --cluster-name $CLUSTER_NAME --namespace $DEFAULT_NAMESPACE --name $DAPR_IDENTITY_NAME --identity-resource-id $DAPR_RESOURCEID
 
 # echo Application name
