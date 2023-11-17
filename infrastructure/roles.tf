c9894d87d61a33b47547fc22f8113947dd8ce713
briandenicola <brian@briandenicola.com>
2023-11-17 16:10:31 -0600

    Missing Role

--- infrastructure/roles.tf
+++ infrastructure/roles.tf
@@ -1,34 +1,48 @@
 resource "azurerm_role_assignment" "aks_role_assignemnt_nework" {
-  scope                = azurerm_virtual_network.traduire_core.id
-  role_definition_name = "Network Contributor"
-  principal_id         = azurerm_user_assigned_identity.aks_identity.principal_id
+  scope                            = azurerm_virtual_network.traduire_core.id
+  role_definition_name             = "Network Contributor"
+  principal_id                     = azurerm_user_assigned_identity.aks_identity.principal_id
   skip_service_principal_aad_check = true
 }
 
 resource "azurerm_role_assignment" "aks_role_assignemnt_msi" {
-  scope                = azurerm_user_assigned_identity.aks_kubelet_identity.id
-  role_definition_name = "Managed Identity Operator"
-  principal_id         = azurerm_user_assigned_identity.aks_identity.principal_id
-  skip_service_principal_aad_check = true 
+  scope                            = azurerm_user_assigned_identity.aks_kubelet_identity.id
+  role_definition_name             = "Managed Identity Operator"
+  principal_id                     = azurerm_user_assigned_identity.aks_identity.principal_id
+  skip_service_principal_aad_check = true
 }
 
 resource "azurerm_role_assignment" "acr_pullrole_node" {
-  scope                     = azurerm_container_registry.traduire_acr.id
-  role_definition_name      = "AcrPull"
-  principal_id              = azurerm_user_assigned_identity.aks_kubelet_identity.principal_id
+  scope                            = azurerm_container_registry.traduire_acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_user_assigned_identity.aks_kubelet_identity.principal_id
   skip_service_principal_aad_check = true
 }
 
 resource "azurerm_role_assignment" "keda_sb_data_owner" {
-  scope                     = azurerm_servicebus_namespace.traduire_app.id
-  role_definition_name      = "Azure Service Bus Data Owner" 
-  principal_id              = azurerm_user_assigned_identity.traduire_identity.principal_id
+  scope                            = azurerm_servicebus_namespace.traduire_app.id
+  role_definition_name             = "Azure Service Bus Data Owner"
+  principal_id                     = azurerm_user_assigned_identity.traduire_identity.principal_id
   skip_service_principal_aad_check = true
 }
 
 resource "azurerm_role_assignment" "dapr_storage_data_reader" {
-  scope                     = azurerm_storage_account.traduire_app.id
-  role_definition_name      = "Storage Blob Data Reader" 
-  principal_id              = azurerm_user_assigned_identity.traduire_identity.principal_id
+  scope                            = azurerm_storage_account.traduire_app.id
+  role_definition_name             = "Storage Blob Data Reader"
+  principal_id                     = azurerm_user_assigned_identity.traduire_identity.principal_id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "secrets" {
+  scope                            = azurerm_key_vault.traduire_app.id
+  role_definition_name             = "Key Vault Secrets User"
+  principal_id                     = azurerm_user_assigned_identity.traduire_identity.principal_id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "certs" {
+  scope                            = azurerm_key_vault.traduire_app.id
+  role_definition_name             = "Key Vault Certificates Officer"
+  principal_id                     = azurerm_user_assigned_identity.traduire_identity.principal_id
   skip_service_principal_aad_check = true
 }
