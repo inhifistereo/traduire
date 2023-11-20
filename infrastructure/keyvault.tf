b70656d6c4104f40bc9a1cb1e3eea79d477d4e05
briandenicola <brian@briandenicola.com>
2023-11-20 11:10:24 -0600

    Moved KeyVault to RBAC

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -6,35 +6,14 @@ resource "azurerm_key_vault" "traduire_app" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
+  enable_rbac_authorization   = true
 
-  sku_name = "standard"
+  sku_name                    = "standard"
 
   network_acls {
     bypass                    = "AzureServices"
     default_action            = "Deny"
     ip_rules                  = ["${chomp(data.http.myip.response_body)}/32"] 
-  }
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = azurerm_user_assigned_identity.traduire_identity.principal_id 
-
-    secret_permissions = [
-      "List",
-      "Get"
-    ]
-  }
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id 
-
-    secret_permissions = [
-      "Set",
-      "Get",
-      "Delete",
-      "List"
-    ]
   }
 }
 
