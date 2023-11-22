bc93b048e91eb6c0f4008e7d3e5aded76c00e94d
briandenicola <brian@briandenicola.com>
2023-11-21 22:12:12 -0600

    Fixed merged regression

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -28,7 +28,7 @@ namespace traduire.webapi
                 {
                     webBuilder.ConfigureKestrel(options =>
                     {
-                        options.ListenAnyIP(80, o => o.Protocols = HttpProtocols.Http2);
+                        options.ListenAnyIP(8080, o => o.Protocols = HttpProtocols.Http2);
                     });
 
                     webBuilder.UseStartup<Startup>();
